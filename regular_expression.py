cfbd9cb1cff9ce6ad0d3745be72e44293b97cc59
annah11 <hanamesfin67@gmail.com>
2024-04-05 04:21:58 +0300

    nested_list

--- regular_expression.py
+++ regular_expression.py
@@ -1,2 +1,10 @@
 from re import*
-match('a','a').group()
+match('[a-z]+','abcd').group()
+# matches the begining of string (^) OR EXCEPT
+match('[^a-z])+','hana').group()
+# matches the end of string ($)
+match('[a-z]+$','abcd').group()
+# matches either regex R or regex S
+match('[a-z]+)|([A-Z]+)','abcD').group()
+# (.) ANY THING  Every character  of  the string matches
+match('.+','abcd').group()
