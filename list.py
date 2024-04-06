5b7be754a6c7506a610d6277b80412614af37f83
annah11 <hanamesfin67@gmail.com>
2024-04-06 07:37:35 +0300

    lst

---
+++ list.py
@@ -0,0 +1,19 @@
+# #itrating alist
+# list=[1,2,3,4,5]
+
+# for x in range(len(list)):
+#     print(list[x])
+# for x in range(len(list)-1,-1,-1):
+#     print(list[x])
+# # whil loop
+#     i=0
+#     while i<len(list):
+#         print(list[i])
+#         i=i+2
+# l1.del
+# l1=[x for x in range(10)]
+# # l1.clear()
+# l2=[x**2 for x in range(1,6)]
+# l3=[x.lower()for x in'Hanna']
+# l4=[x for x in(10,20,30,5,7,9)if x%2==0]
+l5=[x for x in'ab12*&^cs78'if x.isalpha()]
